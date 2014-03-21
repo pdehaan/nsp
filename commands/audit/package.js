2afeffc7f4d9bba4edf675e47961c88930932f31
David Dias <daviddias.p@gmail.com>
2014-03-20 17:12:02 -0700

    thigns

--- commands/audit/package.js
+++ commands/audit/package.js
@@ -2,7 +2,7 @@ var celeri = require('celeri');
 var package = require('./../../lib/package');
 
 celeri.option({
-    command: 'audit-package OR ap',
+    command: 'audit-package',
     description: 'audits your package.json against NSP db'
 }, action);
 
--- commands/audit/shrinkwrap.js
+++ commands/audit/shrinkwrap.js
@@ -2,7 +2,7 @@ var celeri = require('celeri');
 var shrinkwrap = require('./../../lib/shrinkwrap');
 
 celeri.option({
-    command: 'audit-shrinkwrap OR as',
+    command: 'audit-shrinkwrap',
     description: 'audits your `npm shrinkwrap` against NSP db'
 }, action);
 
