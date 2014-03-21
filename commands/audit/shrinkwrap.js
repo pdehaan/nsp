16fed5052a9b2a4aba562db867576afd1b8723e3
David Dias <daviddias.p@gmail.com>
2014-03-20 17:12:21 -0700

    thigns

--- commands/audit/shrinkwrap.js
+++ commands/audit/shrinkwrap.js
@@ -2,7 +2,7 @@ var celeri = require('celeri');
 var shrinkwrap = require('./../../lib/shrinkwrap');
 
 celeri.option({
-    command: 'audit-shrinkwrap',
+    command: 'audit-shrinkwrap OR as',
     description: 'audits your `npm shrinkwrap` against NSP db'
 }, action);
 
